f95690304862e775b6c59be68a803eba18fad0b9
YutoUrushima <yuto259233@gmail.com>
2022-05-15 14:48:08 +0900

    Add error handling statement

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,13 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for named person
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+ 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	// Taking the long way, you might have written this as:
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
